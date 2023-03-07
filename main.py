552b0113f209d2841da0a682af6ef27f45af659b
Jacob Ranz <jacobranz4@gmail.com>
2023-03-06 18:43:45 -0800

    Inital test of app authentication

---
+++ main.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+
+def promptCreds():
+    print("Please enter credentials to continue...")
+    username = input("Username: ")
+    password = input("Password: ")
+    creds = [username, password]
+    return creds
+
+def appLogin(username, password):
+    mydb = mysql.connector.connect(
+        host = "127.0.0.1",
+        user = username,
+        password = password,
+        database = "pass_manager"
+    )
+    return mydb
+    
+def testLogin(database):
+    cursor = database.cursor()
+    cursor.execute('''select * from vault''')
+    test = cursor.fetchall()
+    print(test)
+
+def main():
+    creds = promptCreds()
+    databse = appLogin(creds[0], creds[1])
+    testLogin(databse)
+
+main()
