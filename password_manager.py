1e8474cd60226664126d3f1dcb94a1a3ddb3b951
Jacob Ranz <jacobranz4@gmail.com>
2023-03-08 21:03:44 -0800

    Adding comments

--- password_manager.py
+++ password_manager.py
@@ -1,2 +1,10 @@
+'''
+This file will be the main application where users 
+interact and perform all input. Capabilities such as 
+creating new entries, modifying existing entries,
+and querying entries will all happen through this 
+file.
+'''
+
 def success():
     print("You have successfully authenticated!")
